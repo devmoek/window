c5720088f4b457594ef59d4137cb4bb735be968b
Michael Myers <moek@devmoek.com>
2023-02-13 21:55:03 +0400

    production

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,7 +4,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector),
-              windows = document.querySelectorAll('[data-modal]'); 
+              windows = document.querySelectorAll('[data-modal]'),
+              scroll = calcScroll();
         
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
 
                 modal.style.display = "block";
                 document.body.classList.add('modal-open');
+                document.body.style.marginRight = `${scroll}px`;
             });
         });
 
@@ -28,6 +30,8 @@ const modals = () => {
 
             modal.style.display = "none";
             document.body.classList.remove('modal-open');
+            document.body.style.marginRight = `0px`;
+
         });
 
         modal.addEventListener("click", (e) => {
@@ -38,6 +42,7 @@ const modals = () => {
 
                 modal.style.display = "none";
                 document.body.classList.remove('modal-open');
+                document.body.style.marginRight = `0px`;
             }
         });
     }
@@ -47,6 +52,22 @@ const modals = () => {
             document.querySelector(selector).style.display = "block";
             document.body.classList.add('modal-open');
         }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
